cfef583cb8d969b393f04f3a7a415431490a7ac3
MaximChernyakhovich <maxim.chernyakhovich@outlook.com>
2023-11-27 21:59:54 +0300

    change procedure add_user.sql

--- SQL/procedures/add_user.sql
+++ SQL/procedures/add_user.sql
@@ -1,25 +1,20 @@
-CREATE OR REPLACE PROCEDURE public.add_user(
-    IN tg_id integer, 
-    IN firstname character varying, 
-    IN lastname character varying, 
-    IN tg_nick character varying
-)
-LANGUAGE plpgsql
+CREATE OR REPLACE PROCEDURE public.add_user(IN tg_id integer, IN firstname character varying, IN lastname character varying, IN tg_nick character varying)
+ LANGUAGE plpgsql
 AS $procedure$
 BEGIN
     -- try tran
     BEGIN
-        IF NOT EXISTS (SELECT "ID" FROM users WHERE "ID" = tg_id) THEN
+        IF NOT EXISTS (SELECT id FROM users WHERE id = tg_id) THEN
             -- Вставка строки в таблицу users
-            INSERT INTO users ("ID", "Firstname", "Lastname", "Nickname") 
+            INSERT INTO users (id, firstname, lastname, nickname) 
             VALUES (tg_id, firstname, lastname, tg_nick); 
 
             -- Вставка строки в таблицу balance
-            INSERT INTO balance ("user_id") 
+            INSERT INTO balance (user_id) 
             VALUES (tg_id);
 
             -- Вставка строки в таблицу categories
-            INSERT INTO categories ("user_id", "name_category")
+            INSERT INTO categories (user_id, name_category)
             VALUES (tg_id, 'refill');
 
             RAISE NOTICE '1'; -- Success notice
@@ -35,4 +30,5 @@ BEGIN
             RAISE NOTICE '0'; -- Error notice
     END;
 
-END $procedure$;
+END $procedure$
+;
